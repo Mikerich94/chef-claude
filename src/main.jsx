14061e737b61648db46158fcad528ae2835ee4f1
Mikerich94 <45273195+Mikerich94@users.noreply.github.com>
2026-04-16 14:05:09 -0400

    Adding loading animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import ClaudeRecipe from "./components/ClaudeRecipe";
 import IngredientsList from "./components/IngredientsList";
 import { getRecipeFromChefClaude } from "./ai";
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-  const [recipe, setRecipe] = React.useState("");
+  const [recipe, setRecipe] = useState("");
+
+  const [loading, setLoading] = useState(false);
 
   async function getRecipe() {
+    setLoading(true);
     const recipeMarkdown = await getRecipeFromChefClaude(ingredients);
     setRecipe(recipeMarkdown);
+    setLoading(false);
   }
 
   function addIngredient(formData) {
@@ -32,6 +36,7 @@ export default function Main() {
       {ingredients.length > 0 && (
         <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
       )}
+      {loading && <p>Fetching your recipe... 🍳</p>}
 
       {recipe && <ClaudeRecipe recipe={recipe} />}
     </main>
