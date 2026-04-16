290a0a88dad145c437a1287297eeb79d7043e210
Mikerich94 <45273195+Mikerich94@users.noreply.github.com>
2026-04-16 14:59:53 -0400

    Button hover states

--- src/main.jsx
+++ src/main.jsx
@@ -41,7 +41,7 @@ export default function Main() {
           aria-label="Add ingredient"
           name="ingredient"
         />
-        <button>Add ingredient</button>
+        <button className="add-btn">Add ingredient</button>
       </form>
       {ingredients.length > 0 && (
         <IngredientsList
