2562c03528c2676f46b9da206e3850354a738f75
Mikerich94 <45273195+Mikerich94@users.noreply.github.com>
2026-04-16 15:28:06 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ClaudeRecipe from "./components/ClaudeRecipe";
 import IngredientsList from "./components/IngredientsList";
 import { getRecipeFromChefClaude } from "./ai";
